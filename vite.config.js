aea564dc9994658f292162b58bd23eb39a7615bf
LIsaCorrea98 <pruebaslaura19@gmail.com>
2025-11-10 23:20:27 -0500

    configuracion para su despliegue en render

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Configuración de Vite
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // Esto asegura que todas las rutas funcionen correctamente
+});
